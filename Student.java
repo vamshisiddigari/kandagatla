142a51cfb0c4686d50cef3c37927e16625666e1d
vamshisiddigari <vamshisiddigari234@gmail.com>
2017-09-14 11:06:10 -0700

    My first commit

---
+++ Student.java
@@ -0,0 +1,111 @@
+import java.util.Date;
+import java.lang.String.splite;
+
+/**
+ * Class that stores information about the concrete student
+ * 
+ * DO NOT REMOVE given methods, but you can add new methods/fields/constructor
+ * and change the given methods implementation. For example you can change implementation for equals()
+ * or hashCode() method
+ * 
+ */
+public class Student implements Comparable {
+
+	/**
+	 * student id
+	 */
+	private int id;
+
+	/**
+	 * student name and surname separated by the whitespace for example:
+	 * fullName = "David Luis";
+	 */
+	private String fullName;
+			
+	/**
+	 * student date of birth in "yyyy-MM-dd" format
+	 */
+	private Date birthDate;
+
+	/**
+	 * student average mark
+	 */
+	private double avgMark;
+
+	public Student(int id, String fullName, Date birthDate, double avgMark) {
+		this.id = id;
+		this.fullName = fullName;
+		this.birthDate = birthDate;
+		this.avgMark = avgMark;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		var words = setFullName.toString().split(' ');
+		String fullName ="";
+		this.fullName = fullName;
+	}
+
+	public Date getBirthDate() {
+		
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		String birthDate = "yyyy-MM-dd";
+		SimpleDateFormat sdf = new SimpleDateFormat(birthDate);
+		this.birthDate = birthDate;
+	}
+
+	public double getAvgMark() {
+		return avgMark;
+	}
+
+	public void setAvgMark(double avgMark) {
+		Scanner sc = new Scanner(System.in);
+		boolean flag;
+		for(int 1=0;i<length; i++){
+			do{
+				if(!flag){
+					System.out.println("Enter valid input");
+				}
+				System.out.println('Enter marks of subject "+(i+1)+":");
+				marks[i] = sc.nextInt();
+				flag = false;
+			}
+			while((marks[i]>100) || (marks[i]<0));
+			i++;
+	}
+	}
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+	
+	
+	/**
+	 * DO NOT change this method it will be used during the task check
+	 */
+	@Override
+	public int compareTo(Object o) {
+		Student other = (Student) o;
+		return (this.fullName.compareTo(other.fullName));
+	}
+}
